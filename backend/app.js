b148c72fd63fbe02b6c35b2075bc2662f68ad3f7
Brian <briand.nester@gmail.com>
2021-03-14 20:16:27 -0400

    Database code.

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
 const api = process.env.API_URL;
+const MongoURLUser = process.env.MONGO_URL_USER
 
-app.get(api+'/products', (req, res) => {
-    res.send('Hello API');
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'dresser',
+        image: 'some_url',
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+})
+
+mongoose.connect(MongoURLUser, {useNewUrlParser: true, useUnifiedTopology: true, dbName: 'daintree-database'})
+.then(() => {
+    console.log('Connection Succesful to MongoDB');
+})
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(3000, () => {
-    console.log(api);
     console.log('Server is successfully running on Localhost:3000');
 })
